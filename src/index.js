430db70c537667ea7ee6bcfa28edcd412af218a4
Denis Mikhailov <denis.michailov@gmail.com>
2019-05-21 18:24:37 +1000

    Initial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './index.scss';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById('root')
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
